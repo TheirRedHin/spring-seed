5025a53cfad075a2f0eb58af22e896fdfac0cb89
redhin <redhin@163.com>
2021-02-04 10:19:57 +0800

    初始化项目

---
+++ src/main/java/com/redhin/springseed/SpringSeedApplication.java
@@ -0,0 +1,13 @@
+package com.redhin.springseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSeedApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSeedApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/redhin/springseed/SpringSeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redhin.springseed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSeedApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
