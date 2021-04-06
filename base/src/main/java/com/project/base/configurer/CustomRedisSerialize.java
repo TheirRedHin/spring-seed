eadca66b5a92d7d19aa54bbb32d5a665a034cc35
TheirRedHin <lingpengjin@163.com>
2021-04-06 15:32:00 +0800

    调整 redis 配置

---
+++ base/src/main/java/com/project/base/configurer/CustomRedisSerialize.java
@@ -0,0 +1,63 @@
+package com.project.base.configurer;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationException;
+
+/**
+ * 序列化工具
+ */
+public class CustomRedisSerialize implements RedisSerializer<Object>  {
+
+  /**
+   * 序列化
+   * @param obj
+   * @return
+   */
+  @Override
+  public byte[] serialize(Object obj) throws SerializationException {
+    if(obj == null) {
+      return null;
+    }
+    ObjectOutputStream obi=null;
+    ByteArrayOutputStream bai=null;
+    try {
+      bai=new ByteArrayOutputStream();
+      obi=new ObjectOutputStream(bai);
+      obi.writeObject(obj);
+      byte[] byt=bai.toByteArray();
+      return byt;
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  /**
+   * 反序列化
+   * @param bytes
+   * @return
+   */
+  @Override
+  public Object deserialize(byte[] bytes) throws SerializationException {
+    if(bytes == null) {
+      return null;
+    }
+    ObjectInputStream oii=null;
+    ByteArrayInputStream bis=null;
+    bis=new ByteArrayInputStream(bytes);
+    try {
+      oii=new ObjectInputStream(bis);
+      Object obj=oii.readObject();
+      return obj;
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+}
--- base/src/main/java/com/project/base/configurer/RedisConfig.java
+++ base/src/main/java/com/project/base/configurer/RedisConfig.java
@@ -17,7 +17,6 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableCaching
@@ -32,15 +31,17 @@ public class RedisConfig extends CachingConfigurerSupport {
     Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(
         Object.class);
     ObjectMapper objectMapper = new ObjectMapper();
+
     // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
-    objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+    objectMapper.setVisibility(
+        PropertyAccessor.ALL,
+        JsonAutoDetect.Visibility.ANY);
+
+    // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会跑出异常
     objectMapper.activateDefaultTyping(
         LaissezFaireSubTypeValidator.instance,
         ObjectMapper.DefaultTyping.NON_FINAL,
         JsonTypeInfo.As.WRAPPER_ARRAY);
-    // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会跑出异常
-    objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
-        ObjectMapper.DefaultTyping.NON_FINAL);
 
     jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
     return jackson2JsonRedisSerializer;
@@ -54,13 +55,10 @@ public class RedisConfig extends CachingConfigurerSupport {
     redisTemplate.setConnectionFactory(redisConnectionFactory);
     // 用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
     redisTemplate.setValueSerializer(serializer());
-
-    StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
     // 使用StringRedisSerializer来序列化和反序列化redis的key值
-    redisTemplate.setKeySerializer(stringRedisSerializer);
-
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
     // hash的key也采用String的序列化方式
-    redisTemplate.setHashKeySerializer(stringRedisSerializer);
+    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
     // hash的value序列化方式采用jackson
     redisTemplate.setHashValueSerializer(serializer());
     redisTemplate.afterPropertiesSet();
@@ -70,14 +68,13 @@ public class RedisConfig extends CachingConfigurerSupport {
 
   @Bean
   public CacheManager cacheManager(RedisConnectionFactory factory) {
-    RedisSerializer<String> redisSerializer = new StringRedisSerializer();
     // 配置序列化（解决乱码的问题）
     RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
         // 缓存有效期
         .entryTtl(Duration.ofDays(10))
         // 使用StringRedisSerializer来序列化和反序列化redis的key值
         .serializeKeysWith(
-            RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
+            RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
         // 使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
         .serializeValuesWith(
             RedisSerializationContext.SerializationPair.fromSerializer(serializer()))
